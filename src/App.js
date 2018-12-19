78ecf0880e677227d63b7aa9759cdef31833e2a1
kjm015 <kjm015@yahoo.com>
2018-12-19 03:52:48 -0600

    seize the means of production

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import logo from './stalin.png';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <p>
-                        Edit <code>src/App.js</code> and save to reload.
+                        We have <i>seized</i> the <b>means of production</b>.
                     </p>
                     <a
                         className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Learn React
+                        Join the React Revolution!
                     </a>
                 </header>
             </div>
